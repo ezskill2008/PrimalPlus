551fe9049da22ded4cd43c6e6c8845efee2a63fe
ezskill2008 <97034919+ezskill2008@users.noreply.github.com>
2025-06-21 16:29:25 +0200

    IBL + Fixes

--- Engine/Graphics/Direct3D11/D3D11Shaders.h
+++ Engine/Graphics/Direct3D11/D3D11Shaders.h
@@ -7,10 +7,9 @@ namespace primal::graphics::d3d11::shaders {
 struct engine_shader {
     enum id : u32 {
         fullscreen_triangle_vs = 0,
-        fill_color_ps = 1,
-        post_process_ps = 2,
-        grid_frustums_cs = 3,
-        light_culling_cs = 4,
+        post_process_ps,
+        grid_frustums_cs,
+        light_culling_cs,
         count
     };
 };
